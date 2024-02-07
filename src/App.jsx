4d0c0d08d2cdf038dc13afb3e4b124ed3c5ccbb4
Sara Badini Confalonieri <sara.badiniconfalonieri@gmail.com>
2024-02-07 20:29:34 +0000

    created simple search form in components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap'
 
 import './App.css';
-
-import Carousel from './components/Carousel'
+import SearchForm from './components/searchForm';
+import Carousel from './components/carousel'
 
 function App() {
   return (
     <>
     {/* TODO add nav bar */}
     {/* TODO add search */}
+    <SearchForm />
     <Carousel />
     {/* TODO add search result area */}
     {/* TODO add footer */}
---
+++ src/components/searchForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function SearchForm({ onSearch }) {
+  const [searchArt, setSearchArt] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchArt);
+  };
+
+  return (
+    <form className="form-inline m-3" onSubmit={handleSubmit}>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for artworks"
+          value={searchArt}
+          onChange={(e) => setSearchArt(e.target.value)}
+        />
+        <div className="input-group-append">
+          <button type="submit" className="btn btn-primary">Search</button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
