c8e864303a65be2504cd6aa4e4115eb110142e49
alinakristy <alina.cristi12@gmail.com>
2024-02-07 20:17:43 +0000

    add footer + basic router

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer() {
+
+    return (
+        <div class="container">
+            <footer class="py-3 my-4 footer">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item text-center col-12">
+                        <p>Project created by Alina-Cristi Osipov, Conflalonieri Badini Sara, Xanthe Horner and Najima Musa.</p>
+                    </li>
+                    <li class="nav-item text-center col-12">
+                        <p>
+                            <a href="https://github.com/alinakristy/gallery-groove" target="_blank">Check out the Github Repository</a>
+                        </p>
+                    </li>
+                </ul>
+                <p class="text-center text-body-secondary">
+                    © TheGroovers Team 2024
+                </p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/pages/GalleryHome.jsx
@@ -0,0 +1,13 @@
+import Carousel from '../components/Carousel';
+
+function GalleryHome() {
+    return (
+        <>
+        {/* TODO add search */}
+        <Carousel />
+        {/* TODO add search result area */}
+        </>
+    );
+}
+
+export default GalleryHome;
