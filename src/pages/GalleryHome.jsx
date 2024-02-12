e7ab724f8aca13e148e3391409660cbbafd196ec
alinakristy <alina.cristi12@gmail.com>
2024-02-12 22:22:32 +0000

    fix build out dir

--- src/pages/GalleryHome.jsx
+++ src/pages/GalleryHome.jsx
@@ -2,6 +2,7 @@ import CarouselApi from '../components/CarouselApi';
 import Carousel from '../components/Carousel';
 import ArtworkComponent from '../components/ArtworkComponent';
 import View from './View';
+
 function GalleryHome() {
     return (
         <>
