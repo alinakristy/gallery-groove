b9a4454b2d1559a0689359c4a77bacd2160050ac
alinakristy <alina.cristi12@gmail.com>
2024-02-12 22:24:03 +0000

    replace carousel with carousel api

--- src/pages/GalleryHome.jsx
+++ src/pages/GalleryHome.jsx
@@ -1,13 +1,13 @@
 import CarouselApi from '../components/CarouselApi';
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
 import ArtworkComponent from '../components/ArtworkComponent';
 import View from './View';
 
 function GalleryHome() {
     return (
         <>
-       {/* <CarouselApi /> */}
-       <Carousel />
+       <CarouselApi />
+       {/* <Carousel /> */}
        <ArtworkComponent />
         </>
     );
